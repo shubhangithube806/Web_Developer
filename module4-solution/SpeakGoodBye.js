d6de704cf779d47782f3ce5336b8a41d1e51fd36
Shubhangi <129383586+shubhangithube806@users.noreply.github.com>
2023-04-29 17:22:09 +0530

    Create SpeakGoodBye.js

---
+++ module4-solution/SpeakGoodBye.js
@@ -0,0 +1,15 @@
+(function () {
+
+  var byeSpeaker = {};
+
+  var speakWord = "Good Bye";
+
+  byeSpeaker.speak = function(name) {
+    console.log(speakWord + " " + name);
+  }
+
+  window.byeSpeaker = byeSpeaker;
+
+})();
+
+
